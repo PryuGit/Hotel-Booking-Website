6ff69006b88dc442f0e869ed3d1a21996770e403
Pratyush <shakyapratyush.ps@gmail.com>
2026-01-06 23:37:48 +0545

    recitfied signup and login url

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def signup():
     return render_template('signup.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5010, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
         
